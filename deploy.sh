bc18ac47390fae2f56d07c4781e0a2e917f2bf00
Warunee Dinunzio <dinunziow@gmail.com>
2021-12-02 17:02:26 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
+#if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:waruneeDinunzio/Vue_Covid_Tracker.git master:gh-pages
 
 cd -
